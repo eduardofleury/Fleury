941a1ef4f62d0b7a113faef5a8f304c4a6a41fd9
iouy <uioh>
2016-12-12 09:37:49 -0200

    inicio
    inicio do projeto

---
+++ models/Aluno.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var AlunoSchema = new mongoose.Schema({
+  nome: String,
+  aluno:{
+  	 nome:String,
+         idade: Number,
+         turma: String,
+         turno: String,
+         matricula: Number
+  }
+});
+
+module.exports = mongoose.model('Aluno', AlunoSchema);
