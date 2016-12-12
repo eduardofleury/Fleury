23b817aa8bdee7c8e276a6b55f3c50d0c16900ca
iouy <uioh>
2016-12-12 11:40:00 -0200

    fdsaf

--- models/Carro.js
+++ models/Carro.js
@@ -1,14 +1,11 @@
 var mongoose = require('mongoose');
 
 var CarroSchema = new mongoose.Schema({
-    nome: String,
-        carro:{
-            cor: String,
-            modelo: String,
-            ano: Number,
-            placa: String,
-            revisao: Number
-        }
+    cor: String,
+    modelo: String,
+    ano: Number,
+    placa: String,
+    revisao: Number
 });
 
 module.exports = mongoose.model('Carro', CarroSchema);
