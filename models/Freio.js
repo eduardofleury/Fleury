47d60a1e4ca41e7cf5d5ca233b7b01c54af0dea3
iouy <uioh>
2016-12-15 13:28:56 -0200

    mais atributois de freio

--- models/Freio.js
+++ models/Freio.js
@@ -2,6 +2,9 @@ var mongoose = require('mongoose');
 
 var FreioSchema = new mongoose.Schema({
     nome: String,
+    codigo: String,
+    preco: Number,
+    dataEntrega: String
 });
 
 module.exports = mongoose.model('Freio', FreioSchema);
