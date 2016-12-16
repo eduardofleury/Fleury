336d223663ca602641eb74976b45359ecc58d830
iouy <uioh>
2016-12-16 11:18:21 -0200

    Revert "ajuste"
    This reverts commit 1567940a6ce0fb20c241ebfaebb809089045c30b.

--- routes/usuario.js
+++ routes/usuario.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var auth = require('./auth');
 
-var mongoose = require('mongoose');
 var Usuario = require('../models/Usuario.js');
 
 /* GET /usuario Listagem de usuários. */
