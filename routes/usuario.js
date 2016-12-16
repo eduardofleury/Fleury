1567940a6ce0fb20c241ebfaebb809089045c30b
iouy <uioh>
2016-12-16 10:47:53 -0200

    ajuste

--- routes/usuario.js
+++ routes/usuario.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var auth = require('./auth');
 
+var mongoose = require('mongoose');
 var Usuario = require('../models/Usuario.js');
 
 /* GET /usuario Listagem de usuários. */
